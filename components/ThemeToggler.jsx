5bacf4438cc85d8efb436fe7c9644d1b30da2092
Bapi Mandal <bapimandal12340@gmail.com>
2024-03-21 21:03:19 +0530

    resolve themetoggler

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { MoonIcon, SunIcon } from "lucide-react";
 import { Button } from "./ui/button";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+// import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 const ThemeToggler = () => {
   const { theme, setTheme } = useTheme();
